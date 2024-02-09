6717d6f08d761a8e3ab8f02e5aa728e6a612b5d0
HikaruHonda <hikaru.h.developer@gmail.com>
2024-02-10 01:22:51 +0900

    Initial Commit

---
+++ for wwdc/for_wwdcApp.swift
@@ -0,0 +1,17 @@
+//
+//  for_wwdcApp.swift
+//  for wwdc
+//
+//  Created by 本田輝 on 2024/02/10.
+//
+
+import SwiftUI
+
+@main
+struct for_wwdcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
